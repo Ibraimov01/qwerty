57c4e7f87fcf92e3e8233e8260180cca605b56bc
Atai Ibraimov <atai.ibraimov10@gmail.com>
2022-11-19 00:18:45 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Dialogs from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header';
@@ -9,15 +10,20 @@ import Profile from './components/Profile/Profile';
 
 const  App = () =>  {
   return (
+    <BrowserRouter>
     <div className='app-wrapper'>
 <Header/>
 <Navbar/>
 <div class='app-wrapper-content'>
-<Dialogs/>
-</div>
-{/*<Profile/>*/}
 
-</div>);
+<Routes>
+<Route exact path="/profile" element={<Profile/>}/>
+<Route path="/dialogs" element={<Dialogs/>}/>
+<Route path="/dialogs" element={<Dialogs/>}/>
+</Routes>
+</div>
+</div>
+</BrowserRouter>);
 }
 
 
